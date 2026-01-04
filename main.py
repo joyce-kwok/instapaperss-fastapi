3dff2c08b7c7991be7930860feca1bc7739903b6
amaryllis <joycekwokhy@gmail.com>
2026-01-04 19:41:53 +0800

    Updated endpoint for housekeeping and removed redundant code.

--- main.py
+++ main.py
@@ -24,68 +24,19 @@ CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
 
 base_url = 'https://www.instapaper.com/api/1/'
 batch_size = 8
-existurls = []
-last_update: datetime = datetime.min.replace(tzinfo=timezone.utc)
 
 class HousekeepRequest(BaseModel):
     action: str
-    hours: int = Field(default=None)
-    days: int = Field(default=None)
-    weeks: int = Field(default=None)
-    minutes: int = Field(default=None)
+    amount: int = Field(default=None)
+    bytag: Optional[str] = Field(default=None)
+    removefromarchived: Optional[bool] = Field(default=False)
+    skipstarred: Optional[bool] = Field(default=True)
 
 class loginRequest(BaseModel):
     insta_username: str
     insta_password: str
 
-        
-# def save_new_items_to_pocket(feed_url):
-#     """Save new items from an RSS feed to Pocket in batches.
-    
-#     Args:
-#         feed_url: URL of the RSS feed to process
-#         batch_size: Number of items to send in each batch (default: 6)
-#     """
-#     url = base_url + '/bookmarks/list'
-#     print(f"Checking {feed_url}...")
-#     # print (f"Existing URLs {existurls}...")
-    
-#     try:
-#         feed = feedparser.parse(feed_url)
-#         if not feed.entries:
-#             print("No entries found in feed.")
-#             return
-            
-#         # Process items in reverse order (oldest first)
-#         entries = reversed(feed.entries)
-#         batch = []
-        
-#         for entry in entries:
-#             published_datetime = parsedate_to_datetime(entry.published)
-#             unix_timestamp = int(published_datetime.timestamp())
-#             # print(f"Checking if {entry.link} is a new link... ")
-#             if entry.link not in existurls and published_datetime > last_update:
-#                print(f"{entry.link} is a new link and will be pushed")
-#                print(f"Original Published Time: {entry.published}, Unix Timestamp (in integer): {unix_timestamp}")
-#                batch.append({
-#                 "action": "add",
-#                 "url": entry.link,
-#                 "title": entry.title,
-#                 "time": unix_timestamp
-#                })
-            
-#             if len(batch) >= batch_size:
-#                 _send_batch_to_pocket(batch)
-#                 batch = []
-                
-#         # Send any remaining items in the final partial batch
-#         if batch:
-#             _send_batch_to_pocket(batch)
-            
-#     except Exception as e:
-#         print(f"Error processing feed {feed_url}: {str(e)}")
-
-def save_new_items_to_instapaper(feed_url,source):
+def save_new_items_to_instapaper(feed_url, source, existurls):
     """Save new items from an RSS feed to Pocket in batches.
     
     Args:
@@ -114,31 +65,20 @@ def save_new_items_to_instapaper(feed_url,source):
                print(f"{entry.link} is a new link and will be pushed")
                print(f"Original Published Time: {entry.published}, Unix Timestamp (in integer): {unix_timestamp}")
                tags_obj = [{"name": source}]
-               data = {
+               params = {
                    "url": entry.link,
                    "title": entry.title,
                    "description": entry.summary,
                    "tags": json.dumps(tags_obj)   # other optional params like folder_id, resolve_final_url, etc.
                 }
                session = make_instapaper_client()
-               resp = session.post(url, data=data)
+               resp = session.post(url, data=params)
                print(resp.text)
     except Exception as e:
         print(f"Error processing feed {feed_url}: {str(e)}")
 
-
-# def _send_batch_to_pocket(batch):
-#     """Helper function to send a batch of items to Pocket."""
-#     try:
-#         json_batch = json.dumps(batch)
-#         encoded = urllib.parse.quote(json_batch)
-#         modify(encoded)
-#     except Exception as e:
-#         print(f"Error sending batch to Pocket: {str(e)}")
-
 def search_existing(source):
     urlist = []
-    latest = datetime.min.replace(tzinfo=timezone.utc)
     url = base_url + 'bookmarks/list'
     params = {
         'tag': source,
@@ -146,14 +86,6 @@ def search_existing(source):
     }
     session = make_instapaper_client()
     response = session.post(url, data=params)
-    # print("consumer key is " + CONSUMER_KEY)
-    # print("consumer secret is " + CONSUMER_SECRET)
-    # print("access token is " + ACCESS_TOKEN)
-    # print("token secret is " + ACCESS_TOKEN_SECRET)
-    # print("searching existing saved posts for source: " + source)
-    # print("request parameters: " + str(params))
-    # print("request url: " + url)
-    # print("response text: " + response.text)
     print(f"Calling list bookmarks API to search saved posts, response code is {response.status_code}")
     if response.status_code == 200:
        articles = response.json()
@@ -169,64 +101,68 @@ def search_existing(source):
           print(f"Last updated: {latest}")
     else:
         urlist.append('error')
-    return urlist, latest, response.status_code
+    return urlist, response.status_code
 
-async def retrieve(state):
-    url = base_url + 'get'
+async def retrievehousekeepItems(amount, skipStarred, getArchived, tag):
+    url = base_url + 'bookmarks/list'
+    housekeep_ids = []
     params = {
-        'consumer_key': CONSUMER_KEY,
-        'access_token': ACCESS_TOKEN,
-        'state': state,
-        'sort': 'oldest',
+        'tag': tag,
+        'limit': 500,
+        'folder_id': 'archive' if getArchived else 'unread'
     }
-    async with AsyncClient() as client:
-       response = await client.post(url, json=params)
-       print(f"Calling retrieve API for {state} action, response code is {response.status_code}")
-       return response.json(), response.status_code
-
-async def get_encoded_param(articles, action, delta):
-    temp = []
-    exprange = datetime.now() - delta
-    print(f"Will housekeep items since {exprange}")
-    for article in articles['list'].values():
-        artime = datetime.fromtimestamp(int(article['time_added']))
-        print(f"Article datetime before conversion: {int(article['time_added'])}")
-        print(f"Converted article datetime: {artime}")
-        if artime < exprange and article['favorite'] == '0':
-            obj = {
-                'action': action,
-                'item_id': article['item_id'],
-            }
-            temp.append(obj)
-    json_temp = json.dumps(temp)
-    encoded = urllib.parse.quote(json_temp)
-    return encoded, len(temp)
-
-def modify(encodedparam):
-    url = base_url + 'send'
-    payload = {
-        'consumer_key': CONSUMER_KEY,
-        'access_token': ACCESS_TOKEN,
-        'actions': encodedparam,
-    }
-    response = requests.post(url, params=payload)
-    print(f"Calling modify API to update items, response code is {response.status_code}")
-    print(f"Response body: {response.text}")
-
-async def recall(state, action, freq):
-    articles, status = await retrieve(state)
-    print(f"Retrieved {state} articles response: {articles}")
-    if status == 200:
-       param, length = await get_encoded_param(articles, action, freq)
-       # print(param)
-       print(f"Amount of items to housekeep: {length}")
-       if length > 0:
-          modify(param)
-          return await recall(state, action, freq)
-       else: 
-          return "housekeeping is done"
+    session = make_instapaper_client()
+    response = session.post(url, data=params)
+    print(f"Calling retrieve API for housekeeping, response code is {response.status_code}")
+    if response.status_code == 200:
+       articles = response.json()
+       article_list = [
+        item
+        for item in articles
+        if item.get("type") == "bookmark"
+        and (not skipStarred or item.get("starred") == 0)
+      ]
+       if len(article_list) > 0:
+          temp = article_list[-amount:]
+          for article in temp:
+              housekeep_ids.append(article['bookmark_id'])
+       else:
+          print("No articles found for housekeeping")
     else:
-        return "Housekeeping is interrupted because the Pocket API has reached its usage limit for the hour."  
+        print("Error retrieving articles for housekeeping")
+    return housekeep_ids
+
+async def archive_items(amount, skipStarred, bytag):
+    items = retrievehousekeepItems(amount, skipStarred, False, bytag)
+    print(f"Items to archive: {items}")
+    url = base_url + 'bookmarks/archive'
+    for itemid in items:
+        params = {
+                "bookmark_id": itemid
+               }
+        session = make_instapaper_client()
+        resp = session.post(url, data=params)
+        print("consumer key is " + CONSUMER_KEY)
+        print("consumer secret is " + CONSUMER_SECRET)
+        print("access token is " + ACCESS_TOKEN)
+        print("token secret is " + ACCESS_TOKEN_SECRET)
+        print("searching existing saved posts for source: " + source)
+        print("request parameters: " + str(params))
+        print("request url: " + url)
+        print("response text: " + resp.text)
+    return "Archive completed"  
+
+async def delete_items(amount, skipStarred, removefromarchived, bytag):
+    items = retrievehousekeepItems(amount, skipStarred, removefromarchived, bytag)
+    print(f"Items to delete: {items}")
+    url = base_url + 'bookmarks/delete'
+    for itemid in items:
+        params = {
+                "bookmark_id": itemid
+               }
+        session = make_instapaper_client()
+        resp = session.post(url, data=params)
+    return "Deletion completed"
 
 def authenticate(
     credentials: Annotated[HTTPBasicCredentials, Depends(security)],
@@ -240,10 +176,8 @@ def authenticate(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Basic"},
         )
-    
     app.state.access_token = auth["access_token"]
     app.state.token_secret = auth["access_token_secret"]
-    
     return auth["code"] == 0
 
 
@@ -258,27 +192,19 @@ async def head_item():
 @app.post("/housekeep", response_class=PlainTextResponse)
 async def housekeep(request: HousekeepRequest, verification: bool = Depends(authenticate)):
     if verification:
-        time_delta = timedelta(
-            days=request.days if request.days else 0,
-            hours=request.hours if request.hours else 0,
-            minutes=request.minutes if request.minutes else 0,
-            weeks=request.weeks if request.weeks else 0
-        )
         res = ''
         if request.action == 'archive':
-            res = await recall('unread', 'archive', time_delta)
+            res = await archive_items(request.amount, request.skipstarred, request.bytag)
         elif request.action == 'delete':
-            res = await recall('archive', 'delete', time_delta)
+            res = await delete_items(request.amount, request.skipstarred, request.removefromarchived, request.bytag)
         else:
             return "Invalid request parameters"
-
         return res
     else:
         return "Unauthorized due to incorrect credentials"
 
 @app.get("/save/{source}", response_class=PlainTextResponse)
 async def save_source(source: str, verification: bool = Depends(authenticate)):
-    global existurls, last_update
     """Save specific feed source"""
     print(f"Data source: {source}")
     if verification: 
@@ -288,27 +214,13 @@ async def save_source(source: str, verification: bool = Depends(authenticate)):
             RSS_FEEDS = config['RSS_FEEDS']
        if source not in RSS_FEEDS:
           return f"Invalid source. Available sources: {', '.join(RSS_FEEDS.keys())}"
-       existurls, last_update, code = search_existing(source)
+       existurls, code = search_existing(source)
        if code == 200:
           with concurrent.futures.ThreadPoolExecutor() as executor:
-            list(executor.map(lambda feed: save_new_items_to_instapaper(feed, source), RSS_FEEDS[source]))
+            list(executor.map(lambda feed: save_new_items_to_instapaper(feed, source, existurls), RSS_FEEDS[source]))
           return f"Saved {source} feeds to pocket"
        else:
           return f"Cannot retrieve saved {source} feeds at the moment. Will not update news in this run."
-
-
-
-# @app.get("/adduser", response_class=RedirectResponse)
-# async def redirect_fastapi():
-#     url = base_url + 'oauth/request'
-#     payload = {
-#         'consumer_key': CONSUMER_KEY,
-#         'redirect_uri': "https://pocketapi-to-fastapi.onrender.com"
-#     }
-#     response = requests.post(url, json=payload)
-#     code = response.text.split("=")[-1]
-#     print(f"retrieved code = {code}")
-#     return f"https://getpocket.com/auth/authorize?request_token={code}&redirect_uri=https://pocketapi-to-fastapi.onrender.com/get-token/{code}"
 
 
 def return_token(username, password):
@@ -320,12 +232,12 @@ def return_token(username, password):
     )
 
     # xAuth parameters go in POST body
-    data = {
+    params = {
         "x_auth_username": username,
         "x_auth_password": password,
         "x_auth_mode": "client_auth",
     }
-    resp = oauth.post(url, data=data)
+    resp = oauth.post(url, data=params)
     
     if resp.status_code != 200:
         return {
@@ -349,7 +261,6 @@ def return_token(username, password):
 
 
 def make_instapaper_client():
-
     return OAuth1Session(
         CONSUMER_KEY,
         client_secret=CONSUMER_SECRET,
